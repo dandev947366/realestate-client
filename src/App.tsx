9b79be9e7a98b81e1f12b69c34b630106a13cd3c
Dan <dan.dev947366@gmail.com>
2025-03-09 10:21:02 +0200

    feat(client): add mui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { DatePicker } from 'antd';
+import Button from '@mui/material/Button';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <DatePicker />
+    <Button variant="contained">Hello world</Button>;
+    </>
+      
+  );
+}
+
+export default App;
